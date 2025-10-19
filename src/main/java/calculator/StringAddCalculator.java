ffc70a21242e14813506bee1867c5b4887f1e6c5
kimheera <hrk0295@naver.com>
2025-10-19 21:45:19 +0900

    feat(calculator): null 또는 빈 문자열 처리 기능 구현

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class StringAddCalculator {
+    public static int add(final String text){
+        if(text == null || text.isEmpty()){
+            return 0;
+        }
+
+        return 0; //임시 리턴값
+    }
+}

aca35a063a7f14e2fd2004b388c0362b1df51027
kimheera <hrk0295@naver.com>
2025-10-20 00:07:14 +0900

    fix(calculator): 커스텀 구분자 분리 시 인덱스 수정

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -25,7 +25,7 @@ public class StringAddCalculator {
     private static int addWithCustomDelimiter(String text){
         String customDelimiter = delimiterExtraction(text);
 
-        text = text.substring(text.indexOf('\n')+2);
+        text = text.substring(text.indexOf('\n')+1);
         String[] tokens = text.split(customDelimiter);
 
         return textToInt(tokens);
@@ -54,6 +54,6 @@ public class StringAddCalculator {
     private static String delimiterExtraction(String text){
         int indexOfDel = text.indexOf('\n');
 
-        return text.substring(1, indexOfDel);
+        return text.substring(2, indexOfDel);
     }
 }

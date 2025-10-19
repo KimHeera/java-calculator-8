2cbc0875aa90c08b8ca5e39dd286bd1050bf9707
kimheera <hrk0295@naver.com>
2025-10-19 22:25:41 +0900

    feat(delimiter): 기본 구분자(쉼표, 콜론) 기능 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,11 +1,36 @@
 package calculator;
 
 public class StringAddCalculator {
-    public static int add(final String text){
+    public static int add(final String text) throws IllegalAccessException {
         if(text == null || text.isEmpty()){
             return 0;
         }
 
-        return 0; //임시 리턴값
+        //쉼표 또는 콜론을 구분자로 갖는 문자열
+        String[] tokens = text.split(",|:");
+        int result = textToInt(tokens);
+
+        return result; //임시 리턴값
     }
+
+    private static int textToInt(String[] tokens) throws IllegalAccessException {
+        int sum = 0;
+        for (String token : tokens) {
+            if(token.isEmpty()){
+                continue;
+            }
+
+            int number = Integer.parseInt(token);
+
+            if(number < 0){
+                throw new IllegalAccessException("음수는 입력할 수 없습니다.");
+                //종료해야 함
+            }
+
+            sum += number;
+        }
+
+        return sum;
+    }
+
 }
